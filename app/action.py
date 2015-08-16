1045c9bce6d3845dbb1b7f1ffcbcccdb01f3fef2
Varun Joshi <varunj.1011@gmail.com>
2015-08-17 00:41:00 +0530

    Added some documentation

--- app/action.py
+++ app/action.py
@@ -1,11 +1,17 @@
 import abc
 
 class Action:
+    """An abstract class that all actions will implement
+    """
 
     @abc.abstractmethod
     def get_inputs(self):
+        """All inputs to the action in a list
+        """
         return
 
     @abc.abstractmethod
     def do(self, **inputs):
+        """Executes the action
+        """
         return
--- app/trigger.py
+++ app/trigger.py
@@ -3,6 +3,12 @@ import event, action
 class Trigger:
 
     def __init__(self, event, actions, variable_mappings):
+        """event is the event object that will trigger the action(s)
+           actions is supposed to be a list of actions that will be triggered 
+           by the event
+           variable_mappings is of the form: 
+            { 'action1' : { 'action_input' : 'event_output' } }
+        """   
         self.event = event
         self.actions = actions
         self.variable_mappings = variable_mappings
@@ -10,12 +16,18 @@ class Trigger:
         self.exec_trigger()
         
     def exec_trigger(self):
+        """Sets up an event loop that checks whether the trigger should be
+           executed, and executes it if that is the case.
+        """
         while True:
             if(self.event.check_for_update()):
                 for action in self.actions:
                     action.do(**self.variable_mappings[action])
 
     def test_variable_mappings(self):
+        """Make sure that all inputs to the action are satisfied, and no 
+           inputs are invalid
+        """
         raise NotImplementedError
 
 
