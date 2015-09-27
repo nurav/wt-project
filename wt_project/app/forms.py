2c10a092a626f6f3eef6a78c1663055de62010fa
Varun Joshi <varunj.1011@gmail.com>
2015-09-27 14:51:50 +0530

    Some changes

--- wt_project/app/forms.py
+++ wt_project/app/forms.py
@@ -3,18 +3,31 @@ from eatapp.services import EVENT_NAMES
 
 class SelectEventForm(forms.Form):
 	"""Select event"""
-	events_list = forms.ChoiceField(choices=EVENT_NAMES)
+	select_event = forms.ChoiceField(
+		choices=EVENT_NAMES, 
+		widget=forms.RadioSelect,
+		label="Select an event:"
+	)
+
+	class Media:
+		css = {
+			'all': ()
+		}
+			
 
 
 class SelectActionForm(forms.Form):
 	"""Select action"""
-	actions_list = forms.ChoiceField(choices=EVENT_NAMES)
+	select_action = forms.ChoiceField(
+		choices=EVENT_NAMES,
+		label="Select an action"
+	)
 
 
 class TriggerDescriptionForm(forms.Form):
 	"""docstring for TriggerDescriptionForm"""
 	name = forms.CharField()
-	description = forms.CharField(required=False)
+	description = forms.CharField(widget=forms.Textarea, required=False)
 	shared = forms.BooleanField(required=False)
 		
 
